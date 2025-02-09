dbf62070301132bcc1f8b7722dafc929572d0f28
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2025-02-09 10:10:50 -0600

    Finished api

--- lib/graphql-full-stack-stack.ts
+++ lib/graphql-full-stack-stack.ts
@@ -1,16 +1,110 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_appsync as appsync, aws_dynamodb as dynamodb } from "aws-cdk-lib";
 
 export class GraphqlFullStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new appsync.GraphqlApi(this, "apis-post", {
+      name: "api-to-process-posts",
+      definition: {
+        schema: appsync.SchemaFile.fromAsset("schema/schema.graphql"),
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GraphqlFullStackQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const tablePosts = new dynamodb.Table(this, "posts-table", {
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
+
+    const dataSourcePosts = api.addDynamoDbDataSource(
+      "datasource-posts",
+      tablePosts
+    );
+
+    const funcGetPost = new appsync.AppsyncFunction(this, "func-get-post", {
+      api,
+      name: "getPosts",
+      dataSource: dataSourcePosts,
+      code: appsync.Code.fromInline(`
+        export function request(ctx) {
+          return { operation: 'Scan' };
+        }
+
+        export function response(ctx) {
+          return ctx.result.items;
+        }
+      `),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+    });
+
+    const funcCreatePost = new appsync.AppsyncFunction(
+      this,
+      "func-create-post",
+      {
+        api,
+        name: "createPost",
+        dataSource: dataSourcePosts,
+        code: appsync.Code.fromInline(`
+          export function request(ctx) {
+            return {
+              operation: 'PutItem',
+              key: util.dynamodb.toMapValues({id: util.autoId()}),
+              attributeValues: util.dynamodb.toMapValues(ctx.args.input),
+            };
+          }
+
+          export function response(ctx) {
+            return ctx.result;
+          }
+        `),
+        runtime: appsync.FunctionRuntime.JS_1_0_0,
+      }
+    );
+
+    new appsync.Resolver(this, "resolver-get-post", {
+      api,
+      typeName: "Query",
+      fieldName: "getPosts",
+      code: appsync.Code.fromInline(`
+        export function request(ctx) {
+          return {};
+        }
+
+        export function response(ctx) {
+          return ctx.prev.result;
+        }
+      `),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+      pipelineConfig: [funcGetPost],
+    });
+
+    new appsync.Resolver(this, "resolver-create-post", {
+      api,
+      typeName: "Mutation",
+      fieldName: "createPost",
+      code: appsync.Code.fromInline(`
+        export function request(ctx) {
+          return {};
+        }
+
+        export function response(ctx) {
+          return ctx.prev.result;
+        }
+      `),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+      pipelineConfig: [funcCreatePost],
+    });
+
+    new cdk.CfnOutput(this, "GraphQLAPIURL", {
+      value: api.graphqlUrl,
+    });
+
+    new cdk.CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || "",
+    });
   }
 }
